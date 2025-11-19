dd1fb1262234c00aa0c740800ead9aab56a56297
mohamed <mohamedsalah2025@gmail.com>
2025-11-19 19:42:24 +0200

    add addNote_cubit

---
+++ lib/cubits/add_note_cubit/addnote_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:meta/meta.dart';
+import 'package:note_app/note_model.dart';
+
+part 'addnote_state.dart';
+
+class AddnoteCubit extends Cubit<AddnoteState> {
+  AddnoteCubit() : super(AddnoteInitial());
+addNote(NoteModel note){
+ var notesBox = Hive.box("note_box");
+ notesBox.add<NoteModel>(note);  
+}
+
+}
+
+
